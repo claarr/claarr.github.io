720e37d06f2fcfcf22107bf969f1fd9540842702
claarr <113651004+claarr@users.noreply.github.com>
2022-11-02 14:41:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validasi() {
+    var nama = document.getElementById("nama").value;
+    var email = document.getElementById("email").value;
+    var alamat = document.getElementById("alamat").value;
+    if (nama != "" && email!="" && alamat !="") {
+        return true;
+    }else{
+        alert('Anda harus mengisi data dengan lengkap !');
+    }
+}
